69cf302d4bb3a11df555b1dd3c8e93adfc68b580
Amberley Romo <amberley@stream.me>
2016-12-08 14:00:28 -0600

    day 3 part 2

--- day3/squares.js
+++ day3/squares.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var possibleTriangles =
   fs.readFileSync('instructions.txt', 'utf-8').trim().split('\n')
     .map(function(triangle) {
-      return triangle.trim().split('  ').map(function(side) {
+      return triangle.trim().split(/\s+/).map(function(side) {
         return +side;
       });
     });
@@ -16,4 +16,15 @@ function isTriangle(sides) {
   );
 }
 
+function remap(triangles) {
+  var remap = [];
+  for (var i = 0; i < triangles.length; i+=3) {
+    remap.push([triangles[i][0], triangles[i+1][0], triangles[i+2][0]]);
+    remap.push([triangles[i][1], triangles[i+1][1], triangles[i+2][1]]);
+    remap.push([triangles[i][2], triangles[i+1][2], triangles[i+2][2]]);
+  }
+  return remap;
+}
+
 console.log(possibleTriangles.filter(isTriangle).length);
+console.log(remap(possibleTriangles).filter(isTriangle).length);
