f9d9a7189cdd8d5b8617d5f1d53c7dff894645d0
Amberley Romo <amberleyjohanna@gmail.com>
2017-12-02 12:42:34 -0600

    2017 day 2 refactor

--- 2017/2/index.js
+++ 2017/2/index.js
@@ -3,35 +3,29 @@ const fs = require('fs');
 (function solve() {
   const input = fs.readFileSync('input.txt', 'utf-8').split('\n')
     .map(function(arr) {
-      return arr.split('\t').map(Number);
-    })
+      return arr.split('\t').map(Number).sort((a, b) => b - a);
+    });
 
   console.log({
     part1: minMax(input),
     part2: evenDiv(input)
-  })
+  });
 })()
 
 function minMax(input) {
   return input
-    .map(function(arr) {
-      return arr.sort((a, b) => a - b);
-    })
-    .reduce(function(sum, curr, _, input) {
-      return sum +=  curr[input.length - 1] - curr[0];
+    .reduce((sum, row) => {
+      return sum += Math.max(...row) - Math.min(...row);
     }, 0);
 }
 
 function evenDiv(input) {
   return input
-    .map(function(arr) {
-      return arr.sort((a, b) => b - a);
-    })
-    .reduce(function (sum, curr) {
-      for (let i = 0; i < curr.length - 1; i++) {
-        for (let j = i + 1; j < curr.length; j++) {
-          if (curr[i] % curr[j] === 0) {
-            return sum += curr[i] / curr[j];
+    .reduce((sum, row) => {
+      for (let i = 0; i < row.length - 1; i++) {
+        for (let j = i + 1; j < row.length; j++) {
+          if (row[i] % row[j] === 0) {
+            return sum += row[i] / row[j];
           } 
         }
       }
