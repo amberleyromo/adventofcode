674e6305d00e72524c40296276d866d343ee2fb7
Amberley Romo <amberleyjohanna@gmail.com>
2018-12-01 11:38:34 -0600

    feat(2018/01): part 2 solution

--- 2018/dec-1/index.js
+++ 2018/dec-1/index.js
@@ -2,7 +2,28 @@ const input = require("fs")
   .readFileSync("input.txt", "utf-8")
   .split(/\n/g);
 
+/*
+ * part one
+ */
 const calibrate = input =>
   input.reduce((acc, variance) => acc + parseInt(variance), 0);
 
 console.log(calibrate(input));
+
+/*
+ * part two
+ */
+const duplicate = input => {
+  const seen = new Set();
+  let sum = 0;
+
+  while (true) {
+    for (const current of input) {
+      sum += Number(current);
+      if (seen.has(sum)) return sum;
+      seen.add(sum);
+    }
+  }
+};
+
+console.log(duplicate(input));
